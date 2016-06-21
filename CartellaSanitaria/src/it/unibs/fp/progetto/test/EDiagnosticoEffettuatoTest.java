6ead28e16be83d156427300d9a3aad72fc84807c
gabriM <g.manenti013@studenti.unibs.it>
2016-06-21 20:51:28 +0200

    Aggiunti metodi di test in EDiagnosticoEffettuatoTest

--- CartellaSanitaria/src/it/unibs/fp/progetto/test/EDiagnosticoEffettuatoTest.java
+++ CartellaSanitaria/src/it/unibs/fp/progetto/test/EDiagnosticoEffettuatoTest.java
@@ -21,16 +21,12 @@ public class EDiagnosticoEffettuatoTest {
 		try{
 			ArrayList<Esame> associati = new ArrayList<>();
 			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
-			//Esame radiografia = new Esame("Radiografia");
 			associati.add(radiografia);
 			
 			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
 			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre","Malato", associati, "Penicillina");
 			elencoMalattia.add(polmonite);
-			
-			//EsameDiagnostico ed1 = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
-			//EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(ed1, polmonite);
-			/* EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato((EsameDiagnostico) radiografia, polmonite); */
+		
 			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
 			assertFalse(e1.isEffettuato());
 	}
@@ -43,15 +39,14 @@ public class EDiagnosticoEffettuatoTest {
 	public void setLuogoPossibileTest(){
 		try{
 			ArrayList<Esame> associati = new ArrayList<>();
-			Esame radiografia = new Esame("Radiografia");
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
 			associati.add(radiografia);
 			
 			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
 			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
 			elencoMalattia.add(polmonite);
 			
-			EsameDiagnostico ed1 = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
-			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(ed1, polmonite);
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
 			e1.setLuogo("Bergamo");
 			
 			assertEquals("Bergamo", e1.getLuogo());
@@ -65,15 +60,14 @@ public class EDiagnosticoEffettuatoTest {
 	public void setOraPossibileTest(){
 		try{
 			ArrayList<Esame> associati = new ArrayList<>();
-			Esame radiografia = new Esame("Radiografia");
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
 			associati.add(radiografia);
 			
 			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
 			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
 			elencoMalattia.add(polmonite);
 			
-			EsameDiagnostico ed1 = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
-			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(ed1, polmonite);
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
 			e1.setOra("09:51");
 			
 			assertEquals("09:51", e1.getOra());
@@ -87,15 +81,14 @@ public class EDiagnosticoEffettuatoTest {
 	public void setDataPossibileTest(){
 		try{
 			ArrayList<Esame> associati = new ArrayList<>();
-			Esame radiografia = new Esame("Radiografia");
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
 			associati.add(radiografia);
 			
 			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
 			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
 			elencoMalattia.add(polmonite);
 			
-			EsameDiagnostico ed1 = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
-			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(ed1, polmonite);
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
 			e1.setData(MyTime.creaData("10/01/2012"));
 			
 			String attesa = MyTime.creaData("10/01/2012").toString();
@@ -106,6 +99,98 @@ public class EDiagnosticoEffettuatoTest {
 	catch(IllegalAccessException e){
 			e.printStackTrace();
 		}
+	}
+	
+	@Test
+	public void setLuogoImpossibile(){
+		try {
+			ArrayList<Esame> associati = new ArrayList<>();
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
+			associati.add(radiografia);
+		
+			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
+			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
+			elencoMalattia.add(polmonite);
+		
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
+
+			boolean thrown = false;
+			
+			try{
+				
+				e1.setLuogo("1234");
+			}
+			catch(IllegalArgumentException e){
+				thrown = true;
+			}
+			assertTrue(thrown);
+		
+		}
+		catch(IllegalAccessException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void setOraImpossibile(){
+		try {
+			ArrayList<Esame> associati = new ArrayList<>();
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
+			associati.add(radiografia);
+		
+			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
+			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
+			elencoMalattia.add(polmonite);
+		
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
+
+			boolean thrown = false;
+			
+			try{
+				e1.setOra("09:g");
+			}
+			catch(IllegalArgumentException e){
+				thrown = true;
+			}
+			assertTrue(thrown);
+		
+		}
+		catch(IllegalAccessException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void setDataImpossibile(){
+		try {
+			ArrayList<Esame> associati = new ArrayList<>();
+			EsameDiagnostico radiografia = new EsameDiagnostico("Radiografia", "Focolaio", "Torace");
+			associati.add(radiografia);
+		
+			ArrayList<Malattia> elencoMalattia = new ArrayList<>();
+			Malattia polmonite = new Malattia("Polmonite", MyTime.creaData("01/01/2012"), "Dolori al petto e febbre", "Malato", associati, "Penicillina");
+			elencoMalattia.add(polmonite);
+		
+			EDiagnosticoEffettuato e1 = new EDiagnosticoEffettuato(radiografia, polmonite);
+
+			boolean thrown = false;
+			
+			try{
+				
+				e1.setData(MyTime.creaData("m/m/m"));
+			}
+			catch(IllegalArgumentException e){
+				thrown = true;
+			}
+			assertTrue(thrown);
+		
+		}
+		catch(IllegalAccessException e){
+			e.printStackTrace();
+		}
+		
 	}
 	
 	
