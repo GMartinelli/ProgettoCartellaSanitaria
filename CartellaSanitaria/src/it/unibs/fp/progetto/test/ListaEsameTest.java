da9403d35903369e881a6e4d70c6917deed1e8ab
Giuseppe <gmartinelli024@studenti.unbs.it>
2016-06-21 15:03:27 +0200

    Tests per listaEsame e listaMalattia

---
+++ CartellaSanitaria/src/it/unibs/fp/progetto/test/ListaEsameTest.java
@@ -0,0 +1,42 @@
+package it.unibs.fp.progetto.test;
+
+import static org.junit.Assert.*;
+import it.unibs.fp.progetto.*;
+import org.junit.Test;
+import java.util.ArrayList;
+
+/**
+ * Classe contenente Test JUnit per il controllo della correttezza della Classe ListaEsame
+ * 
+ * 
+ * @author Martinelli Giuseppe
+ */
+
+
+public class ListaEsameTest{
+
+	@Test
+	public void testCercaEsamePresente() {
+		Esame esame1 = new Esame("Radiografia");
+		Esame esame2 = new Esame("Ecografia");
+		ArrayList<Esame> array1 = new ArrayList<Esame>();
+		array1.add(esame1);
+		array1.add(esame2);
+		ListaEsame lista1 = new ListaEsame(array1);
+		boolean trovato = lista1.cercaEsame("Radiografia"); 
+		assertEquals(true, trovato);
+	}
+	
+	@Test
+	public void testCercaEsameAssente() {
+		Esame esame1 = new Esame("Radiografia");
+		Esame esame2 = new Esame("Ecografia");
+		ArrayList<Esame> array1 = new ArrayList<Esame>();
+		array1.add(esame1);
+		array1.add(esame2);
+		ListaEsame lista1 = new ListaEsame(array1);
+		boolean trovato = lista1.cercaEsame("Esame del sangue"); 
+		assertEquals(false, trovato);
+	}
+
+}
