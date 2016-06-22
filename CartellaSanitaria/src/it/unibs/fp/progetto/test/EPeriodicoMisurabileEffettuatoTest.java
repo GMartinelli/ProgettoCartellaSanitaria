d693654c6d5ec4dba9fd41f825f186dfd21f77f6
gabriM <g.manenti013@studenti.unibs.it>
2016-06-22 16:54:46 +0200

    Test setAvvisi() EPeriodicoMisurabileEffettuato

--- CartellaSanitaria/src/it/unibs/fp/progetto/test/EPeriodicoMisurabileEffettuatoTest.java
+++ CartellaSanitaria/src/it/unibs/fp/progetto/test/EPeriodicoMisurabileEffettuatoTest.java
@@ -37,6 +37,7 @@ public class EPeriodicoMisurabileEffettuatoTest {
 		}
 	}
 	
+	@Test
 	public void setAvvisiInferioreTest() {
 		try{
 		ArrayList<Esame> associati = new ArrayList<>();
@@ -48,7 +49,7 @@ public class EPeriodicoMisurabileEffettuatoTest {
 		elencoMalattia.add(diabete);
 	
 		String esito = "Malato";
-		EPeriodicoMisurabileEffettuato e1 = new EPeriodicoMisurabileEffettuato(glicemia,diabete, "Sarnico", MyTime.creaData("02/01/2012"),"08:50",0.50);
+		EPeriodicoMisurabileEffettuato e1 = new EPeriodicoMisurabileEffettuato(glicemia,diabete, "Sarnico", MyTime.creaData("02/01/2012"),"08:50",-10.00);
 		
 		assertEquals("Attenzione! L'esito dell'esame e' inferiore al valore massimo accettabile", e1.getAvvisi());
 		}
@@ -57,6 +58,7 @@ public class EPeriodicoMisurabileEffettuatoTest {
 		}
 	}
 	
+	@Test
 	public void setAvvisiSuperioreTest() {
 		try{
 		ArrayList<Esame> associati = new ArrayList<>();
@@ -70,14 +72,15 @@ public class EPeriodicoMisurabileEffettuatoTest {
 		String esito = "Malato";
 		EPeriodicoMisurabileEffettuato e1 = new EPeriodicoMisurabileEffettuato(glicemia,diabete, "Sarnico", MyTime.creaData("02/01/2012"),"08:50",16.00);
 		
-		assertEquals("Attenzione! L'esito dell'esame e' inferiore al valore massimo accettabile", e1.getAvvisi());
+		assertEquals("Attenzione! L'esito dell'esame e' superiore al valore massimo accettabile", e1.getAvvisi());
 		}
 		catch(IllegalAccessException e){
 			e.printStackTrace();
 		}
 	}
 	
-	public void setAvvisiFuoriSogliaTest() {
+	@Test
+	public void setAvvisiFuoriSogliaMaxTest() {
 		try{
 		ArrayList<Esame> associati = new ArrayList<>();
 		EsamePeriodicoMisurabile glicemia = new EsamePeriodicoMisurabile("Glicemia", 1.00,15.00, 50.00);
@@ -90,7 +93,28 @@ public class EPeriodicoMisurabileEffettuatoTest {
 		String esito = "Malato";
 		EPeriodicoMisurabileEffettuato e1 = new EPeriodicoMisurabileEffettuato(glicemia,diabete, "Sarnico", MyTime.creaData("02/01/2012"),"08:50",51.00);
 		
-		assertEquals("Non bere prima dell'Esame", e1.getAvvisi());
+		assertEquals("URGENZA! L'esito dell'esame e' MOLTO superiore al valore massimo accettabile", e1.getAvvisi());
+		}
+		catch(IllegalAccessException e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void setAvvisiFuoriSogliaMinTest() {
+		try{
+		ArrayList<Esame> associati = new ArrayList<>();
+		EsamePeriodicoMisurabile glicemia = new EsamePeriodicoMisurabile("Glicemia", 1.00,15.00, 50.00);
+		associati.add(glicemia);
+	
+		ArrayList<Malattia> elencoMalattia = new ArrayList<>();
+		Malattia diabete = new Malattia("Diabete", MyTime.creaData("01/01/2012"), "Pressione Alta", "Malato", associati, "Insulina");
+		elencoMalattia.add(diabete);
+	
+		String esito = "Malato";
+		EPeriodicoMisurabileEffettuato e1 = new EPeriodicoMisurabileEffettuato(glicemia,diabete, "Sarnico", MyTime.creaData("02/01/2012"),"08:50",-60.00);
+		
+		assertEquals("URGENZA! L'esito dell'esame e' MOLTO inferiore al valore massimo accettabile", e1.getAvvisi());
 		}
 		catch(IllegalAccessException e){
 			e.printStackTrace();
