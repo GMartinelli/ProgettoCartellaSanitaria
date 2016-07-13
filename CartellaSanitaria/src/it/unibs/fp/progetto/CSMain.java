2feb80b3e2da4bd1db85755c25f5dc421890bf88
Giuseppe <gmartinelli024@studenti.unbs.it>
2016-07-13 13:23:31 +0200

    Cambiate stringhe costanti che non avevano senso (Probabilmente errori battitura)

--- CartellaSanitaria/src/it/unibs/fp/progetto/CSMain.java
+++ CartellaSanitaria/src/it/unibs/fp/progetto/CSMain.java
@@ -128,7 +128,7 @@ public class CSMain implements Serializable{
 	 * @author Martinelli Giuseppe
 	 */
 	public static Esame creaEsame(){
-		String nome = MyInput.leggiStringaNonVuota(P_MEX_INS_NOME);
+		String nome = MyInput.leggiStringaNonVuota(E_MEX_INS_NOME);
 		String raccomandazioni = MyInput.leggiStringa(E_MEX_INS_RACCOMANDAZIONI);
 		int sceltaT = 0;
 		do{
@@ -794,7 +794,7 @@ public class CSMain implements Serializable{
 		
 		sintomi = MyInput.leggiStringaNonVuota(M_MEX_INS_SINTOMI);
 		diagnosi = MyInput.leggiStringaNonVuota(M_MEX_INS_DIAGNOSI);
-		terapia = MyInput.leggiStringaNonVuota(M_MEX_INS_DIAGNOSI);
+		terapia = MyInput.leggiStringaNonVuota(M_MEX_INS_TERAPIA);
 		
 		boolean inserisciEsame = MyInput.yesOrNo(M_SCELTA_INS_ASSOCIATO);
 		String nomeAssociato = null;
